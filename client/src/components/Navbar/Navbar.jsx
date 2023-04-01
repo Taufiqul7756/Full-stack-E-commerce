5fa3e950613b5a08661dab562755f928269b8f71
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-04-01 23:49:57 +0600

    Navbar and Footer added

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left"></div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
