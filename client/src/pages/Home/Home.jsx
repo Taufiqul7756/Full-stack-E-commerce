d51b6eca21b1847887919b8c190e586ad6076599
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-04-03 03:33:55 +0600

    Home Jsx and scss file

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Slider from "../../components/Slider/Slider";
+import "../Home/Home.scss";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <Slider />
+    </div>
+  );
 }
 
 export default Home;
