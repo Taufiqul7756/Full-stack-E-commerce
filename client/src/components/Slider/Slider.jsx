04cc030eaa0dbf72950746f91f240c9ae6bf8d1f
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-04-03 03:33:29 +0600

    Data added and showed to homepage

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
+import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
 
 const Slider = () => {
   const data = [
@@ -6,7 +8,23 @@ const Slider = () => {
     "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
     "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
   ];
-  return <div className="slider">Slider</div>;
+  return (
+    <div className="slider">
+      <div className="container">
+        <img src={data[0]} alt="" />
+        <img src={data[1]} alt="" />
+        <img src={data[2]} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon">
+          <WestOutlinedIcon />
+        </div>
+        <div className="icon">
+          <EastOutlinedIcon />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Slider;
