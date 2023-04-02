6772237d149c45fb335c4b705244ed1e01a946ed
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-04-03 02:04:44 +0600

    Slider section for Home page

---
+++ client/src/components/Slider/Slider.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Slider = () => {
+  const data = [
+    "https://images.pexels.com/photos/1549200/pexels-photo-1549200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/949670/pexels-photo-949670.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/837140/pexels-photo-837140.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ];
+  return <div className="slider">Slider</div>;
+};
+
+export default Slider;
