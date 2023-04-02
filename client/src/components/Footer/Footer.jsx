437aef314a7d3831d2e55b2cb9399e42b589fcb1
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-04-02 21:52:40 +0600

    Footer section's content

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import Payment from "../../images/payment.png";
 
-function Footer() {
-  return <div>Footer</div>;
-}
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span> Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <lorem>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+            consequatur.
+          </lorem>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo
+            blanditiis mollitia suscipit voluptates corporis consequuntur
+            necessitatibus neque accusamus temporibus assumenda.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">LOGO</span>
+          <span className="copyright">@copyright 2023 tawfiq</span>
+        </div>
+        <div className="right">
+          <img src={Payment} alt="Payment" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
