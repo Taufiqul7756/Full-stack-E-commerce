807f53d85cbff329cb4bcf74d34addf1e10370dd
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-04-04 04:52:42 +0600

    Featured Products

---
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FeaturedProducts = () => {
+  return <div>FeaturedProducts</div>;
+};
+
+export default FeaturedProducts;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Slider from "../../components/Slider/Slider";
 import "../Home/Home.scss";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 
 function Home() {
   return (
     <div className="home">
       <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
     </div>
   );
 }
