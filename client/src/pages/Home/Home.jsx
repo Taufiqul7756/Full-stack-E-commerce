d3579737e81f6c37f056c8e42b6dae99339f2fc8
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-05-16 18:00:34 +0600

    Contact added

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Slider from "../../components/Slider/Slider";
 import "../Home/Home.scss";
 import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
 import Categories from "../../components/Categories/Categories";
+import Contact from "../../components/Contact/Contact";
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
       <FeaturedProducts type="featured" />
       <Categories />
       <FeaturedProducts type="trending" />
+      <Contact />
     </div>
   );
 }
