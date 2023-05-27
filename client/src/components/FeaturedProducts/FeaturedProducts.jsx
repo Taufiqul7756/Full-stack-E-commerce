8f513722cae2fbdf9be44d22293405510fb983ee
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-05-28 01:10:12 +0600

    Featured products show from strApi

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,47 +1,13 @@
 import React from "react";
 import Card from "../Card/Card";
 import "./FeaturedProducts.scss";
+import useFetch from "../../hooks/useFetch";
 
 const FeaturedProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/10508388/pexels-photo-10508388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/15776120/pexels-photo-15776120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "T-Shirt",
-      oldPrice: 13,
-      isNew: true,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/15776120/pexels-photo-15776120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/10508388/pexels-photo-10508388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Long Sleeve T-shirt",
-      oldPrice: 13,
-      isNew: true,
-      price: 12,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/5685315/pexels-photo-5685315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/10508388/pexels-photo-10508388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][type][$eq]=${type}`
+  );
 
-      title: "Coat",
-      isNew: false,
-      oldPrice: 13,
-      price: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/11992098/pexels-photo-11992098.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/15776120/pexels-photo-15776120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Panjabi",
-      oldPrice: 13,
-      isNew: false,
-      price: 12,
-    },
-  ];
   return (
     <div className="featuredProducts">
       <div className="top">
@@ -53,9 +19,11 @@ const FeaturedProducts = ({ type }) => {
         </p>
       </div>
       <div className="bottom">
-        {data.map((item) => (
-          <Card item={item} kew={item.id} />
-        ))}
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
       </div>
     </div>
   );
