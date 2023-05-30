37a7508e2382e375b4150e7adf9379b83b7d1e62
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2023-05-30 21:33:35 +0600

    Query for Sub Categories

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -1,52 +1,19 @@
 import React from "react";
 import "../List/List.scss";
 import Card from "../Card/Card";
+import useFetch from "../../hooks/useFetch";
 
-const List = () => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/10508388/pexels-photo-10508388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/15776120/pexels-photo-15776120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "T-Shirt",
-      oldPrice: 13,
-      isNew: true,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/15776120/pexels-photo-15776120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/10508388/pexels-photo-10508388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Long Sleeve T-shirt",
-      oldPrice: 13,
-      isNew: true,
-      price: 12,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/5685315/pexels-photo-5685315.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/10508388/pexels-photo-10508388.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-
-      title: "Coat",
-      isNew: false,
-      oldPrice: 13,
-      price: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/11992098/pexels-photo-11992098.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      img2: "https://images.pexels.com/photos/15776120/pexels-photo-15776120.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      title: "Panjabi",
-      oldPrice: 13,
-      isNew: false,
-      price: 12,
-    },
-  ];
+const List = ({ subCats, maxPrice, sort, catId }) => {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+  );
   return (
     <div className="list">
-      {data?.map((item) => (
-        <Card item={item} key={item.id} />
-      ))}
+      {loading
+        ? "loading"
+        : data?.map((item) => <Card item={item} key={item.id} />)}
     </div>
   );
 };
